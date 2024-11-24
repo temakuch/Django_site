076994577a6bdf5ef49a9c697d01c6eff42f22d1
temakuch <ku4eruck2005@gmail.com>
2024-11-24 12:01:14 +0200

    second commit

--- first_site/main/models.py
+++ first_site/main/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=50)
+    text = models.TextField()
+    date_of_creation = models.DateTimeField(default = timezone.now)
