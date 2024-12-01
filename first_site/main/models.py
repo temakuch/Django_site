df9c971be86865e883cd4f4d520c6fe32e33cfb2
temakuch <ku4eruck2005@gmail.com>
2024-12-01 12:04:33 +0200

    added post list

--- first_site/main/models.py
+++ first_site/main/models.py
@@ -6,3 +6,6 @@ class Post(models.Model):
     title = models.CharField(max_length=50)
     text = models.TextField()
     date_of_creation = models.DateTimeField(default = timezone.now)
+
+    # def __str__(self):
+    #     return f"New_post{self.title}"
--- first_site/main/views.py
+++ first_site/main/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.forms import UserCreationForm
+from .models import Post
+
 def index(request):
     return render(request, 'index.html')
 
@@ -8,8 +10,8 @@ def about(request):
     return render(request, 'about.html')
 
 def show_post(request):
-    
-    return(request, 'post.html')
+    posts = Post.objects.all()
+    return render(request, 'post.html', {'all_posts':posts})
 
 def register(request):
     return render(request, 'register.html')
